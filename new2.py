0bc522bb59abd713963187250238bb225eeac092
xdd <xiaodaidaizyy@163.com>
2020-08-16 14:21:41 +0800

    Second commit

---
+++ new2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn import linear_model
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import neighbors
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler,normalize, MaxAbsScaler
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import  mean_squared_error
+from sklearn.metrics import  r2_score
+from sklearn.model_selection import cross_val_score
+####################################################################
